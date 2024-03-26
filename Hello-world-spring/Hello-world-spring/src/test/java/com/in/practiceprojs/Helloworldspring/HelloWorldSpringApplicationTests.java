03c414a13e1a420ccf85ae591ac71b1a933e5b4f
Ethan2310 <89082288+Ethan2310@users.noreply.github.com>
2024-03-26 11:24:52 +0200

    Add files via upload
    Basic exercise on Spring Boot

---
+++ Hello-world-spring/Hello-world-spring/src/test/java/com/in/practiceprojs/Helloworldspring/HelloWorldSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in.practiceprojs.Helloworldspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
